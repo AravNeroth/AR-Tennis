1d2c6e2f7abcd39fa23a3ce6953f92a6fc8ddd5b
AravNeroth <reacharav@outlook.com>
2023-12-05 15:20:52 -0600

    maybe works?

---
+++ AR TENNIS/AR TENNIS/AR_TENNISApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_TENNISApp.swift
+//  AR TENNIS
+//
+//  Created by neroth_927927 on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AR_TENNISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
