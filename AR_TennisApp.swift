edd5efdd8d2f7f01fe9d7913015415cfa9d7a8db
AravNeroth <reacharav@outlook.com>
2023-12-05 15:17:53 -0600

    base files

---
+++ AR_TennisApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_TennisApp.swift
+//  AR Tennis
+//
+//  Created by neroth_927927 on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AR_TennisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AR Tennis
+//
+//  Created by neroth_927927 on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, AR Tennis!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
